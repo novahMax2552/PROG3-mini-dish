fdc1819f005cb0f2ebef04eb1222edcd69e2a686
novahMax2552 <hei.vahatra@gmail.com>
2025-12-16 15:00:28 +0300

    commit

--- demo/src/main/java/com/example/demo/DBConnection.java
+++ demo/src/main/java/com/example/demo/DBConnection.java
@@ -4,11 +4,11 @@ import java.sql.SQLException;
 import java.sql.Connection;
 
 public class DBConnection {
-    public final String JADBC_URL = "jdbc:postgresql://localhost:5432/mini_dish_db";
+    public final String JDBC_URL = "jdbc:postgresql://localhost:5432/mini_dish_db";
     public final String JDBC_USER = "mini_dish_manager";
     public final String JDBC_PASSWORD = "....";
 
     public Connection getDBConnection() throws Exception {
-        return java.sql.DriverManager.getConnection(JADBC_URL, JDBC_USER, JDBC_PASSWORD);
+        return java.sql.DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
     }
 }
