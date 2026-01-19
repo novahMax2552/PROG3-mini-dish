b0d151e1c45091527e0778fb5d5cafa764f0205d
novahMax2552 <hei.vahatra@gmail.com>
2026-01-19 14:59:06 +0300

    schemas

--- mini_dish_db/schemas.sql
+++ mini_dish_db/schemas.sql
@@ -1,16 +1,17 @@
+CREATE TABLE dish (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    dish_type VARCHAR(50) NOT NULL
+);
+
 CREATE TABLE ingredients (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     price numeric(10,2) NOT NULL,
     category VARCHAR(50) NOT NULL,
     id_dish INT,
-    FOREIGN KEY (id_dish) REFERENCES dishe(id)
-)
+    FOREIGN KEY (id_dish) REFERENCES dish(id)
+);
 
-CREATE TABLE dish (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    dish_type VARCHAR(50) NOT NULL
-)
 
 ALTER TABLE ingredients ADD COLUMN IF NOT EXISTS required_quantity DOUBLE PRECISION NULL;
