4df4b80f0291409fee9d4352e7dee6e1a4588f08
novahMax2552 <hei.vahatra@gmail.com>
2026-01-24 17:57:10 +0300

    review

--- mini_dish_db/schemas.sql
+++ mini_dish_db/schemas.sql
@@ -1,19 +1,17 @@
-CREATE TABLE dish (
+CREATE TYPE category_enum AS ENUM ('VEGETABLE', 'ANIMAL', 'MARINE', 'DAIRY', 'OTHER');
+
+CREATE TYPE dish_type_enum AS ENUM ('START', 'MAIN', 'DESSERT');
+
+CREATE TABLE Dish (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    dish_type VARCHAR(50) NOT NULL
+    dish_type dish_type_enum NOT NULL
 );
 
-CREATE TABLE ingredients (
+CREATE TABLE Ingredient (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    name VARCHAR(100) UNIQUE NOT NULL,
     price NUMERIC(10,2) NOT NULL,
-    category VARCHAR(50) NOT NULL,
-    id_dish INT,
-    FOREIGN KEY (id_dish) REFERENCES dish(id)
+    category category_enum NOT NULL,
+    id_dish INT REFERENCES Dish(id) ON DELETE SET NULL
 );
-
-ALTER TABLE ingredients 
-ADD COLUMN IF NOT EXISTS required_quantity DOUBLE PRECISION NULL;
-ALTER TABLE ingredients 
-ADD COLUMN IF NOT EXISTS unit VARCHAR(20) NULL;
